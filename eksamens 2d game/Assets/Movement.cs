924b8b730cba060f6018be1fb9c8d0a4ce103425
rich0285 <44639744+rich0285@users.noreply.github.com>
2020-04-28 12:46:19 +0200

    init

---
+++ eksamens 2d game/Assets/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public Rigidbody2D rb;
+    public Camera cam;
+    public Animator animator;
+    Vector2 movement;
+    Vector2 mousePos;
+    Vector2 posDif;
+
+    bool facingRight = true;
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        posDif = mousePos - rb.position;
+
+        animator.SetFloat("Mouse Horizontal", posDif.x);
+        animator.SetFloat("Mouse Vertical", posDif.y);
+        animator.SetFloat("Move Horizontal", movement.x);
+        animator.SetFloat("Move Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        // using mousePosition and player's transform (on orthographic camera view)
+        var delta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+       
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    
+     
+}
