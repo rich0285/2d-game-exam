917037c55d711a33e7da807afa83543b54ee7230
ChristianOcchionero <43983111+ChristianOcchionero@users.noreply.github.com>
2020-05-10 17:11:44 +0200

    spawn roomes with lots of bugs
    Biggest Commit : ( but no more proplems with starter room

---
+++ eksamens 2d game/Assets/Scripts DungonRooms/RoomTemplates.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] bottomRooms;
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+
+    public GameObject closedRoom;
+
+    public List<GameObject> rooms;
+
+
+    void Update()
+    {
+
+
+    }
+}
