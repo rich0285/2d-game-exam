7bf4e2f79c52696d91284919cf2859c931b95376
rich0285 <44639744+rich0285@users.noreply.github.com>
2020-05-19 19:16:08 +0200

    Fireball damage stuff

--- eksamens 2d game/Assets/Scripts/Player Scripts/PlayerManager.cs
+++ eksamens 2d game/Assets/Scripts/Player Scripts/PlayerManager.cs
@@ -31,8 +31,8 @@ public class PlayerManager : MonoBehaviour
 
     void OnTriggerCollisionEnter2D(Collision2D collision)
     {
-        Enemy enemy = collision.collider.GetComponent<Enemy>();
-        if (enemy != null)
+       // Enemy enemy = collision.collider.GetComponent<Enemy>();
+      //  if (enemy != null)
         {
             DamageTaken();
         }
