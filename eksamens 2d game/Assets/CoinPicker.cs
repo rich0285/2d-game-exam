d29c82408f12d22c3098a70740ebfbde2b296df6
rich0285 <44639744+rich0285@users.noreply.github.com>
2020-05-19 16:37:57 +0200

    coin

---
+++ eksamens 2d game/Assets/CoinPicker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPicker : MonoBehaviour
+{
+    private float Score = 0f;
+
+    void OnTiggerEnter2D(Collider2D collider)
+    {
+        if (collider.transform.tag == "Coins")
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
