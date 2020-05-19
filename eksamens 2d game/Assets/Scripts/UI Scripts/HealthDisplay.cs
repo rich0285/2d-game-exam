b70cab0359c799998d5319e693542f36c120fd22
rich0285 <44639744+rich0285@users.noreply.github.com>
2020-05-19 15:27:33 +0200

    UI

---
+++ eksamens 2d game/Assets/Scripts/UI Scripts/HealthDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    private int Health = 100;
+    public Text HealthText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthText.text = "Health : " + Health.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Set Hp
+
+    }
+}
