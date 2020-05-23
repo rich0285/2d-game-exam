c420ef51f3846d2054abb51693a75fa9b16bc5b8
ChristianOcchionero <43983111+ChristianOcchionero@users.noreply.github.com>
2020-05-23 17:50:49 +0200

    Fixed Infini Demon Spawn
    Pls remember to keep an eye on hirearchy

--- eksamens 2d game/Assets/Scripts DungonRooms/RoomTemplates.cs
+++ eksamens 2d game/Assets/Scripts DungonRooms/RoomTemplates.cs
@@ -15,16 +15,18 @@ public class RoomTemplates : MonoBehaviour
 
     public float waitTime;
     public GameObject boss;
+    private bool spawnedBoss;
 
     void Update()
     {
-        if (waitTime <= 0)
+        if (waitTime <= 0 && spawnedBoss == false)
         {
             for (int i = 0; i < rooms.Count; i++)
             {
                 if (i == rooms.Count - 1)
                 {
                     Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+                    spawnedBoss = true;
                 }
             }
         }
