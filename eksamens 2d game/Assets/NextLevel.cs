62b51a49b6b52ecfe3ed45cb844654ba4afeb093
rich0285 <44639744+rich0285@users.noreply.github.com>
2020-05-25 13:08:01 +0200

    ladder

---
+++ eksamens 2d game/Assets/NextLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("game Ended");
+
+            SceneManager.LoadScene(0);
+            
+        }
+
+    }
+}
