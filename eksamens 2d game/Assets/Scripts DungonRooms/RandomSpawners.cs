20432f4e42cb186611ba0e0ec630b56aaf69c900
ChristianOcchionero <43983111+ChristianOcchionero@users.noreply.github.com>
2020-05-29 18:00:26 +0200

    Spawners of stuff 1
    a safty commit

---
+++ eksamens 2d game/Assets/Scripts DungonRooms/RandomSpawners.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class RandomSpawners : MonoBehaviour
+{
+    public GameObject Enemy;
+    public GameObject Coin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnStuff", 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnStuff()
+    {
+        int randy = Random.Range(-10, 10);
+        if (randy > 0)
+        {
+            Debug.Log("randy>0");
+            Instantiate(Coin, transform.position, Quaternion.identity);
+            Destroy(gameObject, 2);
+        }
+        if (randy < 0)
+        {
+            Debug.Log("randy<0");
+            Instantiate(Enemy, transform.position, Quaternion.identity);
+            Destroy(gameObject, 2);
+        }
+        else
+        {
+            Debug.Log("=0");
+            Destroy(gameObject, 2);
+        }
+    }
+}
